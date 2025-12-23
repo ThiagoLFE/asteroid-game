3cd56cfa2c197befb2151acca81d39b7248aab26
ThiagoLFE <thiago.rib1905@gmail.com>
2025-12-22 22:20:10 -0300

    feat: rewritten the idea behind draw player and update player to append it's on the container gruop and update and draw the Group

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
+
+        #Function to close modal game when the (x) on the window is pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
